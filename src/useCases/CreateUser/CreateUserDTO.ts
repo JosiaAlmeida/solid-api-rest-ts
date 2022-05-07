27be7e6a8d0d19adba93be12ce56beca7b97e028
JosiaAlmeida <josiaalmeida0001@gmail.com>
2022-05-07 11:13:04 +0100

    Aplicando os principios solidos

---
+++ src/useCases/CreateUser/CreateUserDTO.ts
@@ -0,0 +1,7 @@
+//Classe usado para transferir dados de uma classe para outra
+
+export interface ICreateUserDTO {
+    name: string
+    email: string
+    password: string
+}
--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,6 +1,21 @@
-export class CreateUserUseCase {
-    constructor() { }
-    async execute() {
+import { User } from "../../entities/User";
+import { IUserRepository } from "../../repositories/IUserRepository";
+import { ICreateUserDTO } from "./CreateUserDTO";
 
+export class CreateUserUseCase {
+    constructor(
+        private userRepository: IUserRepository
+    ) {
+
+    }
+    async execute(data: ICreateUserDTO) {
+        const userAlreadyExist = await this.userRepository.findByEmail(data.email)
+
+        if (userAlreadyExist) {
+            throw new Error("User already exists.")
+        }
+        const user = new User(data)
+
+        await this.userRepository.save(user)
     }
 }
